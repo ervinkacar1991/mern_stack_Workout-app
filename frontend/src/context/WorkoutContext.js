9d58858833b9f0de8f27c9a173fdc1b39fe79c46
kacarervin <kacarervin@gmail.com>
2022-12-28 10:37:24 +0100

    Add useReducer hook

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -1,7 +1,16 @@
+import { useReducer } from "react";
 import { createContext } from "react";
 
 export const workoutContext = createContext();
 
+const workoutsReducer = () => {};
+
 export const WorkoutContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(workoutsReducer, {
+    workoust: null,
+  });
+
+  dispatch({ type: "CREATE_WORKOUT" });
+
   return <workoutContext.Provider>{children}</workoutContext.Provider>;
 };
